f814ff25437ff8ad41c08e271249b3b16ecca18a
Rivenn <zafimenasteven@gmail.com>
2025-06-14 23:51:11 +0300

    Update DateTimeConverter to handle nullable DateTime values

--- Arroseo/Converters/DateTimeConverter.cs
+++ Arroseo/Converters/DateTimeConverter.cs
@@ -3,21 +3,27 @@ using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace Arroseo.Converters 
+namespace Arroseo.Converters
 {
-    public class DateTimeConverter : JsonConverter<DateTime>
+    public class DateTimeConverter : JsonConverter<DateTime?>
     {
         private const string DateFormat = "yyyy-MM-dd'T'HH:mm";
 
-        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             var value = reader.GetString();
+            if (string.IsNullOrWhiteSpace(value))
+                return null;
+
             return DateTime.ParseExact(value, DateFormat, CultureInfo.InvariantCulture);
         }
 
-        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue(value.ToString(DateFormat));
+            if (value.HasValue)
+                writer.WriteStringValue(value.Value.ToString(DateFormat));
+            else
+                writer.WriteNullValue();
         }
     }
 }
